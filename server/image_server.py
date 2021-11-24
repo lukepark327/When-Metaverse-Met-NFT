e9d25bdd9c40d26c375b58bf08c3dac3c3c7291b
lukepark327 <lukepark327@gmail.com>
2021-11-24 23:18:48 +0900

    image server init

---
+++ server/image_server.py
@@ -0,0 +1,44 @@
+import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+# from werkzeug.utils import secure_filename
+from Crypto.Hash import keccak
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!\n'
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    img = request.files['file']
+    # filename = secure_filename(img.filename)
+
+    k = keccak.new(digest_bits=256)
+    k.update(img.read())
+    hashed_key = k.hexdigest()
+
+    img.seek(0)
+    img.save(os.path.join('./data', hashed_key))
+
+    return jsonify({'hashed_key': hashed_key})
+
+
+def download():
+    pass
+
+
+def remove():
+    pass
+
+
+if __name__ == '__main__':
+    if not os.path.exists('./data'):
+        os.makedirs('./data')
+
+    app.run(host='0.0.0.0', port=8327)
